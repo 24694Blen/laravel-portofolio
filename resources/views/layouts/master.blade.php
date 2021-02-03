c1e55148e56cc38072db7ec96ccc57641494cba7
Blen <44225561+24694Blen@users.noreply.github.com>
2021-02-03 14:48:28 +0100

    Nav/Footer almost finished
    Made a nav/foorter for desktop view with html and sass
    
    got some fonts en icons to go with it

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,6 +16,8 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/mfd/09b70eb47474836f25a21660282ce0fd/raw/e06a670afcb2b861ed2ac4a1ef752d062ef6b46b/Gilroy.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +31,7 @@
                 <div class="nav__left">
                     <ul class="nav__list">
                         <li class="nav__item">
-                            <a href="">Home</a>
+                            <a href="">@include('images.logo')</a>
                         </li>
                     </ul>
                 </div>
@@ -49,7 +51,17 @@
                         </li>
                     </ul>
                 </div>
+                <div class="nav__right">
+                    <ul class="nav__list">
+                        <li class="nav__item">
+                            <a class="nav__btn nav__btn--style" href="">Give me a Job</a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
+            <div class="header">
+
+            </div>
         </header>
 
         <main>
@@ -59,9 +71,88 @@
 
 
 
-        <footer>
-            <div class="footer">
-                <p>Footer</p>
+        <footer class="footer">
+            <div class="footer__one">
+                <ul class="footer__list">
+                    <li class="footer__item">
+                        <a href="">@include('images.logo')</a>
+                    </li>
+                    <li class="footer__item">
+                        <p class="footer__item--description">
+                            Ik ben jong, gemotiveerd en leergierig. Ik hou van een uitdaging
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__two">
+                <ul class="footer__list">
+                    <li class="footer__item">
+                        <a class="footer__item--text footer__item--title" href="">Sitemap</a>
+                    </li>
+                    <li class="footer__item">
+                        <a class="footer__item--text" href="">Home</a>
+                    </li>
+                    <li class="footer__item">
+                        <a class="footer__item--text" href="">Projects</a>
+                    </li>
+                    <li class="footer__item">
+                        <a class="footer__item--text" href="">About me</a>
+                    </li>
+                    <li class="footer__item">
+                        <a class="footer__item--text" href="">Contact</a>
+                    </li>
+                </ul>
+            </div>
+          
+            <div class="footer__four">
+                <ul class="footer__list">
+                    <li class="footer__item">
+                        <a class="footer__item--text footer__item--title" href="">Contact</a>
+                    </li>
+                    <li class="footer__item">
+                        <p class="footer__item--text">E-mail: <a href="">blen2001@2outlook.com</a></p>
+                    </li>
+                    <li class="footer__item">
+                        <p class="footer__item--text">Tel: <a href="">0628221992</a></p>
+                    </li>
+                    <li class="footer__item">
+                        <p class="footer__item--text">Locatie: unknown</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__three">
+                <ul class="footer__list">
+                    <li class="footer__item">
+                        <a class="footer__item--text footer__item--title" href="">Service</a>
+                    </li>
+                    <li class="footer__item">
+                        <a class="footer__item--text" href="">Nieuws</a>
+                    </li>
+                    <li class="footer__item">
+                        <a class="footer__item--text" href="">Jobs</a>
+                    </li>
+                    <li class="footer__item">
+                        <a class="footer__item--text" href="">Blog</a>
+                    </li>
+                    <li class="footer__item">
+                        <div class="footer__social">
+                            <ul class="footer__list">
+                                <li class="footer__item">
+                                    <a class="footer__btn" href="">@include('images.insta')</a>
+                                </li>
+                                <li class="footer__item">
+                                    <a class="footer__btn" href="">@include('images.facebook')</a>
+                                </li>
+                                <li class="footer__item">
+                                    <a class="footer__btn" href="">@include('images.linkedin')</a>
+                                </li>
+                                <li class="footer__item">
+                                    <a class="footer__btn" href="">@include('images.twitter')</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </footer>
     </div>
